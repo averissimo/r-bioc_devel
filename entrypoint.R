8cc815bd6ae29b25a342361e108cfaebbe95a40d
André Veríssimo <afsverissimo@gmail.com>
2022-01-10 15:48:37 +0100

    adds action

--- entrypoint.R
+++ entrypoint.R
@@ -1,4 +1,10 @@
-setwd('/home/rstudio/glmSparseNet')
+setwd('/home/glmSparseNet')
+
+download.file('https://github.com/sysbiomed/glmSparseNet/archive/refs/heads/master.zip', destfile = 'master.zip')
+
+unzip('master.zip')
+
+setwd('/home/glmSparseNet/glmSparseNet-master')
 
 #BiocManager::install(remotes::local_package_deps(dependencies = TRUE))
 
@@ -8,16 +14,11 @@ BiocManager::install(remotes::local_package_deps(dependencies = TRUE), Ncpus = 4
 
 remotes::install_deps(dependencies = TRUE, Ncpus = 4)
 
-
 cat(glue::glue("Versions:\n {R.Version()$version.string}\n Bioconductor: {BiocManager::version()}\n\n"))
 
 cat("-------------\n\n")
 
 options(crayon.enabled = TRUE)
-
-# download.file('https://github.com/sysbiomed/glmSparseNet/archive/refs/heads/master.zip', destfile = 'master.zip')
-
-# unzip('master.zip')
 
 devtools::check(manual = TRUE, cran = TRUE, check_dir = glue::glue("check_{BiocManager::version()}"), error_on = "warning")
 
